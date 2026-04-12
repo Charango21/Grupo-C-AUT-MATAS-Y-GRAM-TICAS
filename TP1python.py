97657a036e74a602df4e8fca1f42b0f552fd586d
AgusValdebenito <a.valdebenito@alumno.um.edu.ar>
2026-04-12 18:27:32 -0300

    Hice el ejercicio 10

--- TP1python.py
+++ TP1python.py
@@ -1,1 +1,22 @@
+#Ejercicio 10
+def transformar(nombre):
+    resultado = ""
+    mayuscula = True
 
+    for i in nombre:
+        if i == " ":
+            resultado += i
+            mayuscula = True
+        else:
+            if mayuscula:
+                resultado += i.upper()
+                mayuscula = False
+            else:
+                resultado += i.lower()
+    return resultado
+
+nombre_usuario = input("Escribi tu nombre completo: ")
+
+nombre_formateado = transformar(nombre_usuario)
+
+print("Nombre en formato titulo:", nombre_formateado)
